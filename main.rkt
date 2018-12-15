7c1c86ee4789374e89bd811afc4e91ca03c11ab3
Jay McCarthy <jay.mccarthy@gmail.com>
2018-12-15 06:33:26 -0500

    count

--- main.rkt
+++ main.rkt
@@ -15,84 +15,91 @@
 
 (struct card (sc ref vs))
 
-(define (make-memorize the-db completed)
-  (define unsorted
-    (for/list ([l (in-list (file->lines the-db))])
-      (match-define (list (app string->number sc) ref vs) (string-split l "\t"))
-      (card sc ref vs)))
-  (define sorted (sort unsorted >= #:key card-sc))
-  (match-define (cons next after) sorted)
-  
-  (match-define (card score ref vs) next)
-  (define verses (string-split vs NEL))
-  (define verses-words (map (λ (v) (string-split v " ")) verses))
-  (define word-count (apply + (map length verses-words)))
-  (define word-indexes (range word-count))
-  (define shuffled-indexes (shuffle word-indexes))
-  (define how-many-blank (clamp 0 (- word-count score) word-count))
-  (define lost-indexes (take shuffled-indexes how-many-blank))
+(define (make-memorize the-db completed to-do)
+  (let/ec return
+    (when (= completed to-do)
+      (return #f))
+    
+    (define unsorted
+      (for/list ([l (in-list (file->lines the-db))])
+        (match-define (list (app string->number sc) ref vs) (string-split l "\t"))
+        (card sc ref vs)))
+    (define sorted (sort unsorted >= #:key card-sc))
+    (match-define (cons next after) sorted)
 
-  (define (render-card reveal?)
-    (define idx -1)
-    (vappend*
-     #:halign 'left
-     (list* (style 'inverse (text (~a "Completed " completed)))
-            (blank)
-            (style (if reveal? 'bold 'normal) (text ref))
-            (for/list ([words (in-list verses-words)])
-              (vappend2 #:halign 'center
-                        (para* 80
-                               (for/list ([w (in-list words)])
-                                 (set! idx (add1 idx))
-                                 (if (memq idx lost-indexes)
-                                   (style 'bold
-                                          (text
-                                           (if reveal?
-                                             w
-                                             (regexp-replace* #px"\\w" w "-"))))
-                                   (text w))))
-                        (blank))))))  
+    (match-define (card score ref vs) next)
+    (define verses (string-split vs NEL))
+    (define verses-words (map (λ (v) (string-split v " ")) verses))
+    (define word-count (apply + (map length verses-words)))
+    (define word-indexes (range word-count))
+    (define shuffled-indexes (shuffle word-indexes))
+    (define how-many-blank (clamp 0 (- word-count score) word-count))
+    (define lost-indexes (take shuffled-indexes how-many-blank))
 
-  (define base (word #:fps 0.0 #:label "Memorize" #:return (void)))
-  (define hidden
-    (word base
-          #:output (render-card #f)
-          #:event
-          (match-lambda
-            [" " revealed]
-            ["q" #f]
-            [_ hidden])))
-  (define revealed
-    (word base
-          #:output (render-card #t)
-          #:event
-          (match-lambda
-            ["<left>" (save! #f)]
-            ["<right>" (save! #t)]
-            ["q" #f]
-            [_ revealed])))
+    (define (render-card reveal?)
+      (define idx -1)
+      (vappend*
+       #:halign 'left
+       (list* (style 'inverse (text (~a "Completed " completed " of " to-do)))
+              (blank)
+              (style (if reveal? 'bold 'normal) (text ref))
+              (for/list ([words (in-list verses-words)])
+                (vappend2 #:halign 'center
+                          (para* 80
+                                 (for/list ([w (in-list words)])
+                                   (set! idx (add1 idx))
+                                   (if (memq idx lost-indexes)
+                                     (style 'bold
+                                            (text
+                                             (if reveal?
+                                               w
+                                               (regexp-replace* #px"\\w" w "-"))))
+                                     (text w))))
+                          (blank))))))
 
-  (define (save! success?)
-    (define adj (if success? sub1 add1))
-    (define new-sorted
-      (sort (cons (card (adj score) ref vs) after)
-            >= #:key card-sc))
-    (with-output-to-file the-db
-      #:exists 'replace
-      (λ ()
-        (for ([c (in-list new-sorted)])
-          (match-define (card sc ref vs) c)
-          (printf "~a\t~a\t~a\n" sc ref vs))))
+    (define base (word #:fps 0.0 #:label "Memorize" #:return (void)))
+    (define hidden
+      (word base
+            #:output (render-card #f)
+            #:event
+            (match-lambda
+              [" " revealed]
+              ["q" #f]
+              [_ hidden])))
+    (define revealed
+      (word base
+            #:output (render-card #t)
+            #:event
+            (match-lambda
+              ["<left>" (save! #f)]
+              ["<right>" (save! #t)]
+              ["q" #f]
+              [_ revealed])))
 
-    (make-memorize the-db (add1 completed)))
+    (define (save! success?)
+      (define adj (if success? sub1 add1))
+      (define new-sorted
+        (sort (cons (card (adj score) ref vs) after)
+              >= #:key card-sc))
+      (with-output-to-file the-db
+        #:exists 'replace
+        (λ ()
+          (for ([c (in-list new-sorted)])
+            (match-define (card sc ref vs) c)
+            (printf "~a\t~a\t~a\n" sc ref vs))))
 
-  hidden)
+      (make-memorize the-db (add1 completed) to-do))
+
+    hidden))
 
 (module+ main
   (require racket/cmdline
            raart/lux-chaos)
-  (define the-db (command-line #:program "memorize" #:args (db) db))
-  (call-with-chaos
-   (make-raart)
-   (λ ()
-     (fiat-lux (make-memorize the-db 0)))))
+  (define (go! db how-many)
+    (call-with-chaos
+     (make-raart)
+     (λ ()
+       (fiat-lux (make-memorize db 0 how-many)))))
+  (command-line #:program "memorize"
+                #:args (db how-many)
+                (go! db (string->number how-many))))
